40436ad7a4759ad81a31450f1eda67ca7d447c3d
Hakan Demirel <mrhakandmrl@gmail.com>
2022-06-14 13:04:45 +0300

    useState-basics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 function App() {
-  return <h2>Advanced React Tutorial</h2>;
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
 }
 
 export default App;
